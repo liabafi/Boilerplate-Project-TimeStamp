02d97704bc3359d4fd510ae2f5830b23325aef6a
liabafi <62510359+liabafi@users.noreply.github.com>
2024-02-20 09:26:17 +0000

    Modificación código mal escrito.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/api/:date", function (req, res) {
 app.get("/api", (req, res) => {
   res.json({
     unix: new Date().getTime(),
-    utx: new Date().toUTCString(),
+    utc: new Date().toUTCString(),
   });
 });
 
